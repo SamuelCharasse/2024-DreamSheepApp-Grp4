7ac401f35bb020da538f3e2008b27fd7696f9c63
Hasmed <samuel.charasse@edu.univ-fcomte.fr>
2024-05-16 09:15:52 +0200

    chore: Add font-awesome library for menu icons

--- src/components/Menu.vue
+++ src/components/Menu.vue
@@ -4,19 +4,29 @@
   <nav>
     <ul>
       <li>
-        <router-link to="/">Home</router-link>
+        <router-link to="/">
+          <i class="fa fa-home"></i> Home
+        </router-link>
       </li>
       <li>
-        <router-link to="/diary">Diary</router-link>
+        <router-link to="/diary">
+          <i class="fa fa-book"></i> Diary
+        </router-link>
       </li>
       <li>
-        <router-link to="/profile">Profile</router-link>
+        <router-link to="/profile">
+          <i class="fa fa-user"></i> Profile
+        </router-link>
       </li>
       <li>
-        <router-link to="/shop">Shop</router-link>
+        <router-link to="/shop">
+          <i class="fa fa-shopping-cart"></i> Shop
+        </router-link>
       </li>
       <li>
-        <router-link to="/settings">Settings</router-link>
+        <router-link to="/settings">
+          <i class="fa fa-cog"></i> Settings
+        </router-link>
       </li>
     </ul>
   </nav>
