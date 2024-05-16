ff3f94dc87dfdbf4c5bf580cb1ace74f33fea9db
Hasmed <samuel.charasse@edu.univ-fcomte.fr>
2024-05-16 08:48:52 +0200

    feat: Add menu component to App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <script setup>
 import Login from './components/Login.vue';
+import Menu from './components/Menu.vue';
 </script>
 <template>
-  
+  <Menu/>
 </template>
---
+++ src/components/Menu.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+</script>
+<template>
+  <nav>
+    <ul>
+      <li>
+        <router-link to="/">Home</router-link>
+      </li>
+      <li>
+        <router-link to="/diary">Diary</router-link>
+      </li>
+      <li>
+        <router-link to="/profile">Profile</router-link>
+      </li>
+      <li>
+        <router-link to="/shop">Shop</router-link>
+      </li>
+      <li>
+        <router-link to="/settings">Settings</router-link>
+      </li>
+    </ul>
+  </nav>
+</template>
