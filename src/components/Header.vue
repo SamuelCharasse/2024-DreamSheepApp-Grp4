8b42e2df9c258b7773184321defe419636cb9835
Hasmed <samuel.charasse@edu.univ-fcomte.fr>
2024-05-17 16:39:55 +0200

    On recommence tout !

--- src/components/Header.vue
+++
@@ -1,7 +0,0 @@
-<script setup lang="ts">
-import logo from '@/components/icons/IconLogo.vue'
-</script>
-<template>
-  <header class="bg-indigo-900">
-    <logo/>
-  </header>
--- src/pages/index.vue
+++
@@ -1,5 +0,0 @@
-<template>
-    <div class="p-8 text-center">
-      <h1>Page d'accueil</h1>
-    </div>
-  </template>
