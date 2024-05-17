4265e34230cb5150ebc22e78da3a76e38d3e8a98
Hasmed <samuel.charasse@edu.univ-fcomte.fr>
2024-05-17 16:34:16 +0200

    refactor: Add login component to connexion page

--- src/pages/connexion.vue
+++ src/pages/connexion.vue
@@ -1,5 +1,9 @@
+<script setup lang="ts">
+import Login from '@/components/Login.vue';
+</script>
 <template>
     <div class="p-8 text-center">
       <h1>Page de connexion</h1>
+        <Login />
     </div>
-  </template>
+</template>
