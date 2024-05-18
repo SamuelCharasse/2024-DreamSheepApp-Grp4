28cc39d1715e73e03b4f234a24c9a68b5c982c5e
Hasmed <samuel.charasse@edu.univ-fcomte.fr>
2024-05-18 13:47:33 +0200

    refactor: Add Header component to App.vue and update main.css

--- DreamSheep-app/src/App.vue
+++ DreamSheep-app/src/App.vue
@@ -1,8 +1,10 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
+import Header from './components/Header.vue';
 </script>
 
 <template>
+  <Header />
   <nav>
     <RouterLink to="/">Home</RouterLink>
     <RouterLink to="/login">Login</RouterLink>
---
+++ DreamSheep-app/src/components/Header.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+</script>
+
+<template>
+    <div>
+        <img src="" alt="">
+        <h1>Dreamsheep</h1>
+    </div>
+</template>
