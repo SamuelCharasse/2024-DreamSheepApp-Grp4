a1c0e420d12234d63fbaebd1fac15c54a1a3ea9a
Hasmed <samuel.charasse@edu.univ-fcomte.fr>
2024-05-20 09:39:47 +0200

    refactor: Add Menu component to App.vue and update main.css

--- DreamSheep-app/src/App.vue
+++ DreamSheep-app/src/App.vue
@@ -1,13 +1,11 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
 import Header from './components/Header.vue';
+import Menu from './components/Menu.vue';
 </script>
 
 <template>
-  <Header />
-  <nav>
-    <RouterLink to="/">Home</RouterLink>
-    <RouterLink to="/login">Login</RouterLink>
-  </nav>
-  <RouterView />
+  <Header/>
+  <main><RouterView /></main>
+  <Menu/>
 </template>
---
+++ DreamSheep-app/src/components/Menu.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import {RouterLink} from 'vue-router';
+</script>
+
+<template>
+    <nav>
+        <router-link to="/">Home</router-link>
+        <router-link to="/login">Login</router-link>
+    </nav>
+</template>
