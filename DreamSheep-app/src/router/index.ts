4627b57c73c9f5b19d0fab5fb10ef196c0a6e270
Hasmed <samuel.charasse@edu.univ-fcomte.fr>
2024-05-20 09:46:42 +0200

    refactor: Add new routes for profile, shop, settings, and diary views

--- DreamSheep-app/src/router/index.ts
+++ DreamSheep-app/src/router/index.ts
@@ -13,6 +13,26 @@ const router = createRouter({
       name: 'login',
       component: () => import('../views/LoginView.vue')
     },
+    {
+      path: '/profile',
+      name: 'profile',
+      component: () => import('../views/ProfileView.vue')
+    },
+    {
+      path: '/shop',
+      name: 'shop',
+      component: () => import('../views/ShopView.vue')
+    },
+    {
+      path: '/settings',
+      name: 'settings',
+      component: () => import('../views/SettingsView.vue')
+    },
+    {
+      path: '/diary',
+      name: 'diary',
+      component: () => import('../views/DiaryView.vue')
+    }
   ]
 })
 
