6faf60ebcbc03ea0976e465b2b0667a7ad28d6f5
Hasmed <samuel.charasse@edu.univ-fcomte.fr>
2024-05-20 09:43:39 +0200

    refactor: Add new links to navigation bar

--- DreamSheep-app/src/components/Menu.vue
+++ DreamSheep-app/src/components/Menu.vue
@@ -5,6 +5,9 @@ import {RouterLink} from 'vue-router';
 <template>
     <nav>
         <router-link to="/">Home</router-link>
-        <router-link to="/login">Login</router-link>
+        <router-link to="/diary">Diary</router-link>
+        <router-link to="/profile">Profile</router-link>
+        <router-link to="/shop">Shop</router-link>
+        <router-link to="/settings">Settings</router-link>
     </nav>
 </template>
