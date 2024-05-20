82f253edb3e95ef59899378ab59f4cef9aed6e50
Hasmed <samuel.charasse@edu.univ-fcomte.fr>
2024-05-20 11:01:16 +0200

    refactor: Update Menu component with new navigation links and styling

--- DreamSheep-app/src/components/Menu.vue
+++ DreamSheep-app/src/components/Menu.vue
@@ -4,12 +4,12 @@ import {RouterLink} from 'vue-router';
 
 <template>
     <nav class="fixed bottom-0 inset-x-0 bg-indigo-500">
-        <div class="flex justify-around p-4">
-            <router-link to="/" class="mx-2">Home</router-link>
-            <router-link to="/diary" class="mx-2">Diary</router-link>
-            <router-link to="/profile" class="mx-2">Profile</router-link>
-            <router-link to="/shop" class="mx-2">Shop</router-link>
-            <router-link to="/settings" class="mx-2">Settings</router-link>
-        </div>
+        <ul class="flex flex-row">
+            <li><router-link to="/">Home</router-link></li>
+            <li><router-link to="/diary">Diary</router-link></li>
+            <li><router-link to="/profile">Profile</router-link></li>
+            <li><router-link to="/shop">Shop</router-link></li>
+            <li><router-link to="/settings">Settings</router-link></li>
+        </ul>
     </nav>
 </template>
