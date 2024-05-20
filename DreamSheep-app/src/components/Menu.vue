c0f5c74ee15e514b49c0a15453811b657107f9b1
Hasmed <samuel.charasse@edu.univ-fcomte.fr>
2024-05-20 10:49:07 +0200

    refactor: Update navigation bar styling and add new links

--- DreamSheep-app/src/components/Menu.vue
+++ DreamSheep-app/src/components/Menu.vue
@@ -3,13 +3,13 @@ import {RouterLink} from 'vue-router';
 </script>
 
 <template>
-    <nav>
-        <div>
-            <router-link to="/">Home</router-link>
-            <router-link to="/diary">Diary</router-link>
-            <router-link to="/profile">Profile</router-link>
-            <router-link to="/shop">Shop</router-link>
-            <router-link to="/settings">Settings</router-link>
+    <nav class="fixed bottom-0 inset-x-0 bg-white">
+        <div class="flex justify-around p-4">
+            <router-link to="/" class="mx-2">Home</router-link>
+            <router-link to="/diary" class="mx-2">Diary</router-link>
+            <router-link to="/profile" class="mx-2">Profile</router-link>
+            <router-link to="/shop" class="mx-2">Shop</router-link>
+            <router-link to="/settings" class="mx-2">Settings</router-link>
         </div>
     </nav>
 </template>
