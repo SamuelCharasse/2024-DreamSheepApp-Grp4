fad6bc66f4aa2688b66e5e2c7f8bcf658a4fe96e
Hasmed <samuel.charasse@edu.univ-fcomte.fr>
2024-05-20 09:52:31 +0200

    feat: Add new links to navigation bar

--- DreamSheep-app/src/components/Menu.vue
+++ DreamSheep-app/src/components/Menu.vue
@@ -4,10 +4,12 @@ import {RouterLink} from 'vue-router';
 
 <template>
     <nav>
-        <router-link to="/">Home</router-link>
-        <router-link to="/diary">Diary</router-link>
-        <router-link to="/profile">Profile</router-link>
-        <router-link to="/shop">Shop</router-link>
-        <router-link to="/settings">Settings</router-link>
+        <div>
+            <router-link to="/">Home</router-link>
+            <router-link to="/diary">Diary</router-link>
+            <router-link to="/profile">Profile</router-link>
+            <router-link to="/shop">Shop</router-link>
+            <router-link to="/settings">Settings</router-link>
+        </div>
     </nav>
 </template>
