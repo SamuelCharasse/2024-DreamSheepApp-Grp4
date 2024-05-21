8c1902128b0cbe3dc824528df8ee168c5ba081b9
Hasmed <samuel.charasse@edu.univ-fcomte.fr>
2024-05-21 14:16:50 +0200

    feat: Add new icons for menu items

---
+++ DreamSheep-app/src/components/icons/ProfileIcon.vue
@@ -0,0 +1,11 @@
+<svg width="43" height="42" viewBox="0 0 43 42" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <ellipse cx="13.2723" cy="28.8446" rx="6.58444" ry="7.00675" transform="rotate(1.13066 13.2723 28.8446)" fill="#D9D9D9"/>
+    <ellipse cx="15.7628" cy="22.0849" rx="6.58444" ry="7.00675" transform="rotate(1.13066 15.7628 22.0849)" fill="#D9D9D9"/>
+    <ellipse cx="26.4359" cy="22.0849" rx="6.58444" ry="7.00675" transform="rotate(1.13066 26.4359 22.0849)" fill="#D9D9D9"/>
+    <ellipse cx="29.6378" cy="29.2003" rx="6.58444" ry="7.00675" transform="rotate(1.13066 29.6378 29.2003)" fill="#D9D9D9"/>
+    <ellipse cx="20.9805" cy="32.4023" rx="6.58444" ry="7.00675" transform="rotate(1.13066 20.9805 32.4023)" fill="#D9D9D9"/>
+    <ellipse cx="21.3941" cy="12.4275" rx="7.85146" ry="8.35504" transform="rotate(1.13066 21.3941 12.4275)" fill="#FEF08A"/>
+    <ellipse cx="13.6721" cy="8.3289" rx="2.15374" ry="3.87111" transform="rotate(34 13.6721 8.3289)" fill="#FEF08A"/>
+    <ellipse cx="29.0164" cy="8.33285" rx="2.15374" ry="3.87111" transform="rotate(-34 29.0164 8.33285)" fill="#FEF08A"/>
+    </svg>
+    
