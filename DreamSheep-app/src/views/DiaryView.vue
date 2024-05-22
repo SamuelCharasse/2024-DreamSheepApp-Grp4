6f7640d87b3b09d2d769cc2268ac0333ed88c33d
Lola Roth <lola.roth@edu.univ-fcomte.fr>
2024-05-22 20:52:32 +0200

    Création Card home

--- DreamSheep-app/src/views/DiaryView.vue
+++ DreamSheep-app/src/views/DiaryView.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
 import CardDiary from '@/components/CardDiary.vue';
+
 </script>
 <template>
     <div class="diary">
       <h1>This is a diary page</h1>
       <CardDiary/>
+      
     </div>
 </template>
--- DreamSheep-app/src/views/HomeView.vue
+++ DreamSheep-app/src/views/HomeView.vue
@@ -1,5 +1,9 @@
+<script setup lang="ts">
+import CardHome from '@/components/CardHome.vue';
+</script>
 <template>
     <div class="login">
       <h1>This is a home page</h1>
+      <CardHome/>
     </div>
 </template>
