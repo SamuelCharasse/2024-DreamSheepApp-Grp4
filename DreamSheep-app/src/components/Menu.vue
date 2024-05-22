07ff5a031e2dc555e8298cd94165f7f1e0fb6dc7
Hasmed <samuel.charasse@edu.univ-fcomte.fr>
2024-05-22 09:49:03 +0200

    refactor: Update Menu component with improved navigation links styling

--- DreamSheep-app/src/components/Menu.vue
+++ DreamSheep-app/src/components/Menu.vue
@@ -8,35 +8,35 @@ import {RouterLink} from 'vue-router';
             <li>
                 <router-link to="/" class="flex flex-col items-center">
                 <img src="./icons/HomeIcon.svg" alt="Icon" class="h-6 w-6 text-slate-50">
-                <img v-if="$route.path === '/'" src="./icons/EllipseMenu.svg" alt="Ellispe" class="h-3 w-3">
+                <img v-if="$route.path === '/'" src="./icons/EllipseMenu.svg" alt="Ellispe" class="h-4 w-4">
                 <span v-else>Home</span>
                 </router-link>
             </li>
             <li>
                 <router-link to="/diary" class="flex flex-col items-center">
                 <img src="./icons/DiaryIcon.svg" alt="Icon" class="h-6 w-6 text-slate-50">
-                <img v-if="$route.path === '/diary'" src="./icons/EllipseMenu.svg" alt="Ellispe" class="h-3 w-3">
+                <img v-if="$route.path === '/diary'" src="./icons/EllipseMenu.svg" alt="Ellispe" class="h-4 w-4">
                 <span v-else>Diary</span>
                 </router-link>
             </li>
             <li>
                 <router-link to="/profile" class="flex flex-col items-center">
                 <img src="./icons/ProfileIcon.svg" alt="Icon" class="h-6 w-6 text-slate-50">
-                <img v-if="$route.path === '/profile'" src="./icons/EllipseMenu.svg" alt="Ellispe" class="h-3 w-3">
+                <img v-if="$route.path === '/profile'" src="./icons/EllipseMenu.svg" alt="Ellispe" class="h-4 w-4">
                 <span v-else>Profile</span>
                 </router-link>
             </li>
             <li>
                 <router-link to="/shop" class="flex flex-col items-center">
                 <img src="./icons/ShopIcon.svg" alt="Icon" class="h-6 w-6 text-slate-50">
-                <img v-if="$route.path === '/shop'" src="./icons/EllipseMenu.svg" alt="Ellispe" class="h-3 w-3">
+                <img v-if="$route.path === '/shop'" src="./icons/EllipseMenu.svg" alt="Ellispe" class="h-4 w-4">
                 <span v-else>Shop</span>
                 </router-link>
             </li>
             <li>
                 <router-link to="/settings" class="flex flex-col items-center">
                 <img src="./icons/SettingsIcon.svg" alt="Icon" class="h-6 w-6 text-slate-50">
-                <img v-if="$route.path === '/settings'" src="./icons/EllipseMenu.svg" alt="Ellispe" class="h-3 w-3">
+                <img v-if="$route.path === '/settings'" src="./icons/EllipseMenu.svg" alt="Ellispe" class="h-4 w-4">
                 <span v-else>Settings</span>
                 </router-link>
             </li>
