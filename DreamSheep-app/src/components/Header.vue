153969ce0e619c2d903cb2b524d75255091cbff0
Hasmed <samuel.charasse@edu.univ-fcomte.fr>
2024-05-23 08:37:50 +0200

    refactor: Remove unused DreamSheep logo SVG and update Header component to use DreamSheep_Logo_Vector component

--- DreamSheep-app/src/components/Header.vue
+++ DreamSheep-app/src/components/Header.vue
@@ -1,9 +1,10 @@
 <script setup lang="ts">
+import DreamSheep_Logo_Vector from '@/components/icons/DreamSheep_Logo_vector.vue';
 </script>
 
 <template>
     <div class="flex items-center justify-center p-4 gap-4 bg-indigo-900">
-        <img src="./icons/DreamSheep_Logo_Vector.svg" alt="Logo DreamSheep">
+        <DreamSheep_Logo_Vector/>
         <h1 class="text-slate-50">Dreamsheep</h1>
     </div>
 </template>
