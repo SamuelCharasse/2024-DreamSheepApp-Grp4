f0eda89484f1ca0794b815bc7a67dcd24c8e2599
Hasmed <samuel.charasse@edu.univ-fcomte.fr>
2024-05-23 09:46:26 +0200

    refactor: Update PageHeader component to use icons for each page

--- DreamSheep-app/src/components/PageHeader.vue
+++ DreamSheep-app/src/components/PageHeader.vue
@@ -1,24 +1,33 @@
 <script setup lang="ts">
+import { RouterLink } from 'vue-router';
+import NewDreamIcon from '@/components/icons/NewDreamIcon.vue';
+import StatisticIcon from './icons/StatisticIcon.vue';
+import NotificationUpIcon from './icons/NotificationUpIcon.vue';
+import NotificationIcon from './icons/NotificationIcon.vue';
 </script>
 
 <template>
-    <div v-if="$route.path === '/'" class="bg-blue-950 text-slate-50">
+    <div v-if="$route.path === '/'" class="flex p-2 bg-blue-950 text-slate-50 justify-around items-center">
         <h2>home page</h2>
-        <!-- les icons là -->
+        <!-- <div class="flex items-center"> faut finir de mettre en forme ici  -->
+            <NewDreamIcon/>
+            <NotificationIcon/>
+        </div>
     </div>
-    <div v-if="$route.path === '/diary'" class="bg-blue-950 text-slate-50">
+    <div v-if="$route.path === '/diary'" class="flex p-2 bg-blue-950 text-slate-50 justify-around items-center">
         <h2>diary page</h2>
-        <!-- les icons là -->
+        <NewDreamIcon/>
+        <StatisticIcon/>
     </div>
-    <div v-if="$route.path === '/profile'" class="bg-blue-950 text-slate-50">
+    <div v-if="$route.path === '/profile'" class="flex p-2 bg-blue-950 text-slate-50 justify-around items-center">
         <h2>profile page</h2>
         <!-- les icons là -->
     </div>
-    <div v-if="$route.path === '/shop'" class="bg-blue-950 text-slate-50">
+    <div v-if="$route.path === '/shop'" class="flex p-2 bg-blue-950 text-slate-50 justify-around items-center">
         <h2>shop page</h2>
         <!-- les icons là -->
     </div>
-    <div v-if="$route.path === '/settings'" class="bg-blue-950 text-slate-50">
+    <div v-if="$route.path === '/settings'" class="flex p-2 bg-blue-950 text-slate-50 justify-around items-center">
         <h2>settings page</h2>
         <!-- les icons là -->
     </div>
