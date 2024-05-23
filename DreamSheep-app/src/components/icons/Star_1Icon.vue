20ca178667d037d41b0404b4c1dd2a11c1b238dc
Hasmed <samuel.charasse@edu.univ-fcomte.fr>
2024-05-23 09:00:54 +0200

    Adding news icons in project

---
+++ DreamSheep-app/src/components/icons/Star_1Icon.vue
@@ -0,0 +1,5 @@
+
+<template><svg width="32" height="32" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg">
+<ellipse cx="15.3192" cy="16" rx="4.42553" ry="16" fill="#FEF08A"/>
+<ellipse cx="16" cy="16.6808" rx="4.42553" ry="16" transform="rotate(-90 16 16.6808)" fill="#FEF08A"/>
+</svg></template>
