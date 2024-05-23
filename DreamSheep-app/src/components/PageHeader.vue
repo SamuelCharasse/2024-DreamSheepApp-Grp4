e370fa89d67976ef611f6cab5026204b19074df2
Hasmed <samuel.charasse@edu.univ-fcomte.fr>
2024-05-23 08:54:41 +0200

    refactor: Update PageHeader component to use more descriptive headers for each page

--- DreamSheep-app/src/components/PageHeader.vue
+++ DreamSheep-app/src/components/PageHeader.vue
@@ -3,23 +3,23 @@
 
 <template>
     <div v-if="$route.path === '/'" class="bg-blue-950 text-slate-50">
-        <h2>En tête de la home page</h2>
+        <h2>home page</h2>
         <!-- les icons là -->
     </div>
     <div v-if="$route.path === '/diary'" class="bg-blue-950 text-slate-50">
-        <h2>En tête de la diary page</h2>
+        <h2>diary page</h2>
         <!-- les icons là -->
     </div>
     <div v-if="$route.path === '/profile'" class="bg-blue-950 text-slate-50">
-        <h2>En tête de la profile page</h2>
+        <h2>profile page</h2>
         <!-- les icons là -->
     </div>
     <div v-if="$route.path === '/shop'" class="bg-blue-950 text-slate-50">
-        <h2>En tête de la shop page</h2>
+        <h2>shop page</h2>
         <!-- les icons là -->
     </div>
     <div v-if="$route.path === '/settings'" class="bg-blue-950 text-slate-50">
-        <h2>En tête de la settings page</h2>
+        <h2>settings page</h2>
         <!-- les icons là -->
     </div>
 </template>
