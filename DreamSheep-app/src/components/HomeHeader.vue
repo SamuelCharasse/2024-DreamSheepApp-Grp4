dfe65b1a28341f680abb54990694c3c9744dbd3a
Hasmed <samuel.charasse@edu.univ-fcomte.fr>
2024-05-23 08:18:05 +0200

    refactor: Add HomeHeader component to HomeView

---
+++ DreamSheep-app/src/components/HomeHeader.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts">
+</script>
+
+<template>
+    <div>
+        <h2>En tête de la home page</h2>
+    </div>
+</template>
--- DreamSheep-app/src/views/HomeView.vue
+++ DreamSheep-app/src/views/HomeView.vue
@@ -1,10 +1,11 @@
 <script setup lang="ts">
 import CardHome from '@/components/CardHome.vue';
 import CardDreamFav from '@/components/CardDreamFav.vue';
+import HomeHeader from '@/components/HomeHeader.vue';
 </script>
 <template>
     <div class="login">
-      <h1>This is a home page</h1>
+      <HomeHeader/>
       <CardDreamFav/>
       <CardHome/>
     </div>
