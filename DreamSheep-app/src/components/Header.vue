4d2a9c16a7febd79a8136d384dd0ff8bed3d84ff
Hasmed <samuel.charasse@edu.univ-fcomte.fr>
2024-05-27 16:08:30 +0200

    refactor: Update PageHeader component to use fixed positioning for each page

--- DreamSheep-app/src/components/Header.vue
+++ DreamSheep-app/src/components/Header.vue
@@ -3,7 +3,7 @@ import DreamSheep_Logo_Vector from '@/components/icons/DreamSheep_Logo_vector.vu
 </script>
 
 <template>
-    <div class="fixed w-full flex items-center justify-center p-4 gap-4 bg-indigo-900">
+    <div class="flex items-center justify-center p-4 gap-4 bg-indigo-900">
         <DreamSheep_Logo_Vector/>
         <h1 class="text-slate-50">Dreamsheep</h1>
     </div>
--- DreamSheep-app/src/components/PageHeader.vue
+++ DreamSheep-app/src/components/PageHeader.vue
@@ -8,31 +8,31 @@ import Star_1Icon from './icons/Star_1Icon.vue';
 </script>
 
 <template>
-    <div v-if="$route.path === '/'" class="fixed w-full mt-[84px] flex p-2 h-16 bg-blue-950 text-slate-50 justify-around items-center">
+    <div v-if="$route.path === '/'" class="flex p-2 h-16 bg-blue-950 text-slate-50 justify-around items-center">
         <h2>Communauté</h2>
         <div class="flex items-center gap-4"> <!-- faut finir de mettre en forme ici  -->
             <NewDreamIcon/>
             <NotificationIcon/>
         </div>
     </div>
-    <div v-if="$route.path === '/diary'" class="fixed w-full mt-[84px] flex p-2 h-16 bg-blue-950 text-slate-50 justify-around items-center">
+    <div v-if="$route.path === '/diary'" class="flex p-2 h-16 bg-blue-950 text-slate-50 justify-around items-center">
         <h2>diary page</h2>
         <div class="flex items-center gap-4">
             <NewDreamIcon/>
             <StatisticIcon/>
         </div>
     </div>
-    <div v-if="$route.path === '/profile'" class="fixed w-full mt-[84px] flex p-2 h-16 bg-blue-950 text-slate-50 justify-around items-center">
+    <div v-if="$route.path === '/profile'" class="flex p-2 h-16 bg-blue-950 text-slate-50 justify-around items-center">
         <h2>profile page</h2>
         <!-- les icons là -->
         <Star_1Icon/>
     </div>
-    <div v-if="$route.path === '/shop'" class="fixed w-full mt-[84px] flex p-2 h-16 bg-blue-950 text-slate-50 justify-around items-center">
+    <div v-if="$route.path === '/shop'" class="flex p-2 h-16 bg-blue-950 text-slate-50 justify-around items-center">
         <h2>shop page</h2>
         <!-- les icons là -->
         <Star_1Icon/>
     </div>
-    <div v-if="$route.path === '/settings'" class="fixed w-full mt-[84px] flex p-2 h-16 bg-blue-950 text-slate-50 justify-around items-center">
+    <div v-if="$route.path === '/settings'" class="flex p-2 h-16 bg-blue-950 text-slate-50 justify-around items-center">
         <h2>settings page</h2>
         <!-- les icons là -->
         <Star_1Icon/>
