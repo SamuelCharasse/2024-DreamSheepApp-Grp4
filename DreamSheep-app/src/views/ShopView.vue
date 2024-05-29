95a12ac40945531726a76a0a7290124ce1705b06
Lola Roth <lola.roth@edu.univ-fcomte.fr>
2024-05-29 14:30:16 +0200

    Page de shop à modifier

--- DreamSheep-app/src/views/ShopView.vue
+++ DreamSheep-app/src/views/ShopView.vue
@@ -1,25 +1,51 @@
 <script setup lang="ts">
 import ShopPremiumIcon from "@/components/icons/ShopPremiumIcon.vue";
 import DownArrowIcon from "@/components/icons/DownArrowIcon.vue";
+import ProfileIcon from "@/components/icons/ProfileIcon.vue";
+import Button from "@/components/Button.vue";
 </script>
 
 <template>
-<main class="m-3">
-  <div class="relative inline-block  text-center">
-  <details class="group">
-    <summary class="inline-flex justify-center w-full rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50 focus:outline-none cursor-pointer">
-      Dropdown button
-      <DownArrowIcon class="-mr-1 ml-2 h-4 w-4"/>
-    </summary>
-    <ul class="origin-top-right absolute mt-2 w-40 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 hidden group-open:block">
-      <li>
-        <a href="#" class="block pl-0.5 pr-0.5 py-2 text-sm text-gray-700">Premium</a>
-      </li>
-        <li>
-            <a href="#" class="block pl-0.5 pr-0.5 py-2 text-sm text-gray-700">Ressources supplémentaires</a>
-        </li>
-    </ul>
-    </details>
+  <main class="m-3">
+    <div class="flex justify-between pb-4">
+      <div class="relative inline-block text-center">
+        <details class="group">
+          <summary
+            class="inline-flex justify-center w-full rounded-md border-2 border-indigo-300 shadow-sm px-4 py-2 bg-white text-sm font-medium text-black hover:bg-gray-50 focus:outline-none cursor-pointer"
+          >
+            Personnalisation
+            <DownArrowIcon class="-mr-1 mt-0.5 ml-2 h-4 w-4" />
+          </summary>
+          <ul
+            class="origin-top-right absolute mt-2 w-40 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 hidden group-open:block"
+          >
+            <li>
+              <RouterLink
+                to="/shop/premium"
+                class="block pl-0.5 pr-0.5 py-2 text-sm font-normal text-gray-700"
+                >Premium</RouterLink
+              >
+            </li>
+          </ul>
+        </details>
+      </div>
+      <ShopPremiumIcon class="mt-2" />
     </div>
-</main>
+    <div
+      class="grid grid-cols-3 cols-span-3 bg-DarkPurple rounded-lg justify-items-center px-2 py-3"
+    >
+      <ProfileIcon />
+      <ProfileIcon />
+      <ProfileIcon />
+      <ProfileIcon />
+      <ProfileIcon />
+      <ProfileIcon />
+      <ProfileIcon />
+      <ProfileIcon />
+      <ProfileIcon />
+    </div>
+    <div class="flex justify-center pt-4">
+      <Button text="Enregistrer" variant="yellow"></Button>
+    </div>
+  </main>
 </template>
