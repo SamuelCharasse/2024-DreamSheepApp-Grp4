eba0717213772eac963ca7c1ffa57f27918c3498
Hasmed <samuel.charasse@edu.univ-fcomte.fr>
2024-05-29 14:19:43 +0200

    refactor: Add "more-info" route to router and update link in SettingsView

---
+++ DreamSheep-app/src/views/SettingsPages/SettingsMoreView.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import { RouterLink } from 'vue-router';
+import RightArrowIcon from '@/components/icons/RightArrowIcon.vue';
+import BackArrowIconsvg from '@/components/icons/BackArrowIconsvg.vue';
+</script>
+
+<template>
+    
+</template>
