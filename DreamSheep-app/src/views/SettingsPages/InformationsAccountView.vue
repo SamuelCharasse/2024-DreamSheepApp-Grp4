f706e2d2050dc96b724dad1c1b5ab3528dce4e3e
Hasmed <samuel.charasse@edu.univ-fcomte.fr>
2024-05-29 10:42:46 +0200

    refactor: Update InformationsAccountView.vue to use text-slate-50 class for labels

--- DreamSheep-app/src/views/SettingsPages/InformationsAccountView.vue
+++ DreamSheep-app/src/views/SettingsPages/InformationsAccountView.vue
@@ -15,39 +15,39 @@ import RightArrowIcon from "@/components/icons/RightArrowIcon.vue";
     </div>
 
     <div class="border-y border-slate-100 py-5">
-      <p class="pb-2.5">Prénom</p>
+      <p class="pb-2.5 text-slate-50">Prénom</p>
       <p class="bg-white text-black rounded-md p-2.5">
         Prénom de l'utilisateur
       </p>
     </div>
 
     <div class="border-y border-slate-100 py-5">
-      <p class="pb-2.5">Nom</p>
+      <p class="pb-2.5 text-slate-50">Nom</p>
       <p class="bg-white text-black rounded-md p-2.5">Nom de l'utilisateur</p>
     </div>
 
     <div class="border-y border-slate-100 py-5">
-      <p class="pb-2.5">Email</p>
+      <p class="pb-2.5 text-slate-50">Email</p>
       <p class="bg-white text-black rounded-md p-2.5">Email de l'utilisateur</p>
     </div>
 
     <div class="border-y border-slate-100 py-5">
-      <p class="pb-2.5">Téléphone</p>
+      <p class="pb-2.5 text-slate-50">Téléphone</p>
       <p class="bg-white text-black rounded-md p-2.5">
         Num tel de l'utilisateur
       </p>
     </div>
 
     <div class="pt-5">
-      <div class="flex px-2.5 pb-2.5 justify-between">
+      <div class="flex px-2.5 pb-2.5 text-slate-50 justify-between">
         <div class="flex-grow flex items-center">
           <ExitIcon class="items-start" />
-          <h3 class="text-left ml-2">Se déconnecter</h3>
+          <RouterLink to="/" class="text-left ml-2">Se déconnecter</RouterLink>
         </div>
         <RightArrowIcon class="items-end" />
       </div>
       <div class="px-2.5 flex-grow">
-        <p class="ml-8">
+        <p class="ml-8 text-slate-50">
           Déconnectez-vous, vos données et vos informations seront conservées.
         </p>
       </div>
