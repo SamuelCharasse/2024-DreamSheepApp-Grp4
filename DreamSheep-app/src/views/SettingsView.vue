bb2ac1a9b53de29a3ec0af87dda0174e01e901cf
Hasmed <samuel.charasse@edu.univ-fcomte.fr>
2024-05-29 09:45:29 +0200

    Refactor : settings page - adding icons

--- DreamSheep-app/src/views/SettingsView.vue
+++ DreamSheep-app/src/views/SettingsView.vue
@@ -6,50 +6,57 @@ import MoneyIcon from '@/components/icons/MoneyIcon.vue';
 import MoreIcon from '@/components/icons/MoreIcon.vue';
 import LogOutIcon from '@/components/icons/LogOutIcon.vue';
 import CrossSettingsIcon from '@/components/icons/CrossSettingsIcon.vue';
+import RightArrowIcon from '@/components/icons/RightArrowIcon.vue';
 </script>
 
 <template>
     <nav>
         <ul class="text-slate-50 h-screen flex flex-col justify-between p-4 mb-20">
             <li>
-                <div class="flex gap-2">
+                <div class="flex gap-2 items-center">
                     <ProfileSettingsIcon/>
                     <router-link class="font-bold text-lg" to="/">Informations du compte</router-link>
+                    <RightArrowIcon/>
                 </div>
                 <p>Affichez les informations de votre compte, comme votre numéro de téléphone et votre adresse mail.</p>
             </li>
             <li>
-                <div class="flex gap-2">
+                <div class="flex gap-2 items-center">
                     <KeyIcon/>
                     <router-link class="font-bold text-lg" to="/">Changer de mot de passe</router-link>
+                    <RightArrowIcon/>
                 </div>
                 <p>Changez de mot de passe à tout moment.</p>
             </li>
             <li>
-                <div class="flex gap-2">
+                <div class="flex gap-2 items-center">
                     <MoneyIcon/>
-                    <router-link class="font-bold text-lg" to="/">Premium</router-link> 
+                    <router-link class="font-bold text-lg" to="/">Premium</router-link>
+                    <RightArrowIcon/> 
                 </div>
                 <p>Gérez les fonctionnalités de votre abonnement, notamment la période d'annulation</p>
             </li>
             <li>
-                <div class="flex gap-2">
+                <div class="flex gap-2 items-center">
                     <MoreIcon/>
                     <router-link class="font-bold text-lg" to="/">Ressources supplémentaires</router-link>
+                    <RightArrowIcon/>
                 </div>
                 <p>Obtenez des informations sur la gestion de vos données et l'application.</p>
             </li>
             <li>
-                <div class="flex gap-2">
+                <div class="flex gap-2 items-center">
                     <LogOutIcon/>
                     <router-link class="font-bold text-lg" to="/">Se déconnecter</router-link>
+                    <RightArrowIcon/>
                 </div>
                 <p>Déconnectez-vous de votre compte. Vos données et vos informations seront conservées</p>
             </li>
             <li>
-                <div class="flex gap-2">
+                <div class="flex gap-2 items-center">
                     <CrossSettingsIcon/>
                     <router-link class="font-bold text-lg" to="/">Supprimer le compte</router-link>
+                    <RightArrowIcon/>
                 </div>
                 <p>Supprimez votre compte. Vos données et vos informations seront supprimées</p>
             </li>
