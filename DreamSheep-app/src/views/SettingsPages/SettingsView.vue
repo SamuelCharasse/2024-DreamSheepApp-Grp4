d0e2a5f1e2a2324ce3c486790eaa649a4d1104cf
Hasmed <samuel.charasse@edu.univ-fcomte.fr>
2024-05-29 10:14:54 +0200

    refactor: Add RouterLink to BackArrowIconsvg in InformationsAccountView.vue and SettingsView.vue

--- DreamSheep-app/src/views/SettingsPages/SettingsView.vue
+++ DreamSheep-app/src/views/SettingsPages/SettingsView.vue
@@ -15,48 +15,54 @@ import RightArrowIcon from '@/components/icons/RightArrowIcon.vue';
             <li>
                 <div class="flex gap-2 items-center">
                     <ProfileSettingsIcon/>
-                    <router-link class="font-bold text-lg" to="/info">Informations du compte</router-link>
-                    <RightArrowIcon/>
+                    <router-link class="flex gap-2 items-center font-bold text-lg" to="/info">Informations du compte
+                        <RightArrowIcon/>
+                    </router-link>
                 </div>
                 <p>Affichez les informations de votre compte, comme votre numéro de téléphone et votre adresse mail.</p>
             </li>
             <li>
                 <div class="flex gap-2 items-center">
                     <KeyIcon/>
-                    <router-link class="font-bold text-lg" to="/">Changer de mot de passe</router-link>
-                    <RightArrowIcon/>
+                    <router-link class="flex gap-2 items-center font-bold text-lg" to="/">Changer de mot de passe
+                        <RightArrowIcon/>
+                    </router-link>                    
                 </div>
                 <p>Changez de mot de passe à tout moment.</p>
             </li>
             <li>
                 <div class="flex gap-2 items-center">
                     <MoneyIcon/>
-                    <router-link class="font-bold text-lg" to="/">Premium</router-link>
-                    <RightArrowIcon/> 
+                    <router-link class="flex gap-2 items-center font-bold text-lg" to="/">Premium
+                        <RightArrowIcon/>
+                    </router-link>                    
                 </div>
                 <p>Gérez les fonctionnalités de votre abonnement, notamment la période d'annulation</p>
             </li>
             <li>
                 <div class="flex gap-2 items-center">
                     <MoreIcon/>
-                    <router-link class="font-bold text-lg" to="/">Ressources supplémentaires</router-link>
-                    <RightArrowIcon/>
+                    <router-link class="flex gap-2 items-center font-bold text-lg" to="/">Ressources supplémentaires
+                        <RightArrowIcon/>
+                    </router-link>                   
                 </div>
                 <p>Obtenez des informations sur la gestion de vos données et l'application.</p>
             </li>
             <li>
                 <div class="flex gap-2 items-center">
                     <LogOutIcon/>
-                    <router-link class="font-bold text-lg" to="/">Se déconnecter</router-link>
-                    <RightArrowIcon/>
+                    <router-link class="flex gap-2 items-center font-bold text-lg" to="/">Se déconnecter
+                        <RightArrowIcon/>
+                    </router-link>
                 </div>
                 <p>Déconnectez-vous de votre compte. Vos données et vos informations seront conservées</p>
             </li>
             <li>
                 <div class="flex gap-2 items-center">
                     <CrossSettingsIcon/>
-                    <router-link class="font-bold text-lg" to="/">Supprimer le compte</router-link>
-                    <RightArrowIcon/>
+                    <router-link class="flex gap-2 items-center font-bold text-lg" to="/">Supprimer le compte
+                        <RightArrowIcon/>
+                    </router-link>
                 </div>
                 <p>Supprimez votre compte. Vos données et vos informations seront supprimées</p>
             </li>
