1359415ed0d3ab779d593f3e863bcadbe83a0050
Hasmed <samuel.charasse@edu.univ-fcomte.fr>
2024-05-29 14:33:51 +0200

    refactor: Update SettingsMoreView.vue to add additional resources section

--- DreamSheep-app/src/views/SettingsPages/SettingsMoreView.vue
+++ DreamSheep-app/src/views/SettingsPages/SettingsMoreView.vue
@@ -5,5 +5,30 @@ import BackArrowIconsvg from '@/components/icons/BackArrowIconsvg.vue';
 </script>
 
 <template>
-    
+    <main class="flex flex-col text-slate-50 p-4 mb-20">
+        <div>
+            <RouterLink to="/settings">
+                <BackArrowIconsvg/>
+            </RouterLink> 
+            <h2>Ressources supplémentaires</h2>
+        </div>
+        <nav>
+            <ul>
+                <li>
+                    <div>
+                        <RouterLink to="/">Conditions d'utilisation</RouterLink>
+                        <RightArrowIcon/>
+                    </div>
+                    <p>Apprenez-en plus sur DreamSheep et son équipe !</p>
+                </li>
+                <li>
+                    <div>
+                        <RouterLink to="/">Contact</RouterLink>
+                        <RightArrowIcon/>
+                    </div>
+                    <p>Vous avez une question ? Contactez-nous !</p>
+                </li>
+            </ul>
+        </nav>
+    </main>
 </template>
