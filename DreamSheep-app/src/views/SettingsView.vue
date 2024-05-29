4cc10c129fc5e2fdc15d5bb51da88919655bb2c1
Hasmed <samuel.charasse@edu.univ-fcomte.fr>
2024-05-29 09:15:18 +0200

    refactor: Add icons to SettingsView navigation links

--- DreamSheep-app/src/views/SettingsView.vue
+++ DreamSheep-app/src/views/SettingsView.vue
@@ -1,32 +1,56 @@
 <script setup lang="ts">
 import { RouterLink } from 'vue-router';
+import ProfileSettingsIcon from '@/components/icons/ProfileSettingsIcon.vue';
+import KeyIcon from '@/components/icons/KeyIcon.vue';
+import MoneyIcon from '@/components/icons/MoneyIcon.vue';
+import MoreIcon from '@/components/icons/MoreIcon.vue';
+import LogOutIcon from '@/components/icons/LogOutIcon.vue';
+import CrossIcon from '@/components/icons/CrossIcon.vue';
 </script>
 
 <template>
     <nav>
         <ul class="text-slate-50 h-screen flex flex-col justify-between p-4 mb-20">
             <li>
-                <router-link class="font-bold text-lg" to="/">Informations du compte</router-link>
+                <div class="flex gap-2">
+                    <ProfileSettingsIcon/>
+                    <router-link class="font-bold text-lg" to="/">Informations du compte</router-link>
+                </div>
                 <p>Affichez les informations de votre compte, comme votre numéro de téléphone et votre adresse mail.</p>
             </li>
             <li>
-                <router-link class="font-bold text-lg" to="/">Changer de mot de passe</router-link>
+                <div class="flex gap-2">
+                    <KeyIcon/>
+                    <router-link class="font-bold text-lg" to="/">Changer de mot de passe</router-link>
+                </div>
                 <p>Changez de mot de passe à tout moment.</p>
             </li>
             <li>
-                <router-link class="font-bold text-lg" to="/">Premium</router-link>
+                <div class="flex gap-2">
+                    <MoneyIcon/>
+                    <router-link class="font-bold text-lg" to="/">Premium</router-link> 
+                </div>
                 <p>Gérez les fonctionnalités de votre abonnement, notamment la période d'annulation</p>
             </li>
             <li>
-                <router-link class="font-bold text-lg" to="/">Ressources supplémentaires</router-link>
+                <div class="flex gap-2">
+                    <MoreIcon/>
+                    <router-link class="font-bold text-lg" to="/">Ressources supplémentaires</router-link>
+                </div>
                 <p>Obtenez des informations sur la gestion de vos données et l'application.</p>
             </li>
             <li>
-                <router-link class="font-bold text-lg" to="/">Se déconnecter</router-link>
+                <div class="flex gap-2">
+                    <LogOutIcon/>
+                    <router-link class="font-bold text-lg" to="/">Se déconnecter</router-link>
+                </div>
                 <p>Déconnectez-vous de votre compte. Vos données et vos informations seront conservées</p>
             </li>
             <li>
-                <router-link class="font-bold text-lg" to="/">Supprimer le compte</router-link>
+                <div class="flex gap-2">
+                    <CrossIcon/>
+                    <router-link class="font-bold text-lg" to="/">Supprimer le compte</router-link>
+                </div>
                 <p>Supprimez votre compte. Vos données et vos informations seront supprimées</p>
             </li>
         </ul>
