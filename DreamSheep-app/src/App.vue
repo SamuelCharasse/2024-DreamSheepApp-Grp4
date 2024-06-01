999ca33393236226039170d4ef14c66f18fcc63e
Lola Roth <lola.roth@edu.univ-fcomte.fr>
2024-06-01 23:49:54 +0200

    commit nul y'a rien qui fontionne

--- DreamSheep-app/src/App.vue
+++ DreamSheep-app/src/App.vue
@@ -3,6 +3,7 @@ import { RouterLink, RouterView } from 'vue-router'
 import Header from './components/Header.vue';
 import Menu from './components/Menu.vue';
 import PageHeader from '@/components/PageHeader.vue';
+
 </script>
 
 <template>
--- DreamSheep-app/src/views/ProfileView.vue
+++ DreamSheep-app/src/views/ProfileView.vue
@@ -1,4 +1,16 @@
+<script setup lang="ts">
+import { SignUp } from "@/assets/backend.mjs";
+import type { users } from "@/type.ts";
+
+
+</script>
+
 <template>
     <div class="profile">
+        <p><label>E-mail</label><input type="email" id="email"/></p>
+        <p><label>Password</label><input type="password" id="Password"/></p>
+        <p><label>Name</label><input type="text" id="name"/></p>
+        <p><button v-on:click="SignUp()">Sign Up</button></p>
+        <h1>test</h1>
     </div>
 </template>
