6a2005302ca864456e8aa7450320903a6de95250
Hasmed <samuel.charasse@edu.univ-fcomte.fr>
2024-06-04 10:44:45 +0200

    refactor: Remove unused RouterLink import in App.vue

--- DreamSheep-app/src/App.vue
+++ DreamSheep-app/src/App.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
+import { RouterView } from 'vue-router'
 import Header from './components/Header.vue';
 import Menu from './components/Menu.vue';
 import PageHeader from '@/components/PageHeader.vue';
