6264f33ed2ed0ae12721872f7d01de398c202476
Lola Roth <lola.roth@edu.univ-fcomte.fr>
2024-06-07 19:01:06 +0200

    cgt

--- DreamSheep-app/src/components/CardDiary.vue
+++ DreamSheep-app/src/components/CardDiary.vue
@@ -7,7 +7,7 @@ import TagIcon from '@/components/icons/TagIcon.vue'
 const props = defineProps<{
     id: string
     title: string
-    content: string
+    description: string
     tags: string[]
     date: string
 }>()
@@ -21,21 +21,14 @@ const props = defineProps<{
         </div>
         <div class="p-3">
             <h3 class="text-black overflow-auto">{{title}}</h3>
-            <p class="text-black text-base overflow-auto">{{content}}</p>
+            <p class="text-black text-base overflow-auto">{{description}}</p>
         </div>
         <div class="flex justify-start items-center flex-grow-0 flex-shrink-0 relative gap-5 px-2 py-1 ">
             <div class="bg-violet-200 rounded-lg flex items-center space-x-1 px-2 py-1">
             <TagIcon/>
-            <p class=" text-black text-xs">{{ tags[0] }}</p>
+            <p class=" text-black text-xs overflow-auto ">{{ tags }}</p>
             </div>
-            <div class="bg-violet-200 rounded-lg flex items-center space-x-1 px-2 py-1">
-            <TagIcon/>
-            <p class=" text-black text-xs">{{tags[1]}}</p>
-            </div>
-            <div class="bg-violet-200 rounded-lg flex items-center space-x-1 px-2 py-1">
-            <TagIcon/>
-            <p class=" text-black text-xs">{{tags[2]}}</p>
-            </div>
+            
         </div>
         <div class="flex justify-start items-center flex-grow-0 flex-shrink-0 relative gap-5 px-2 py-1 pb-2.5">
             <div class="flex items-center space-x-1 px-2 py-1">
--- DreamSheep-app/src/views/DiaryView.vue
+++ DreamSheep-app/src/views/DiaryView.vue
@@ -1,17 +1,33 @@
 <script setup lang="ts">
+import { ref, onMounted } from 'vue';
 import CardDiary from '@/components/CardDiary.vue';
 import { pb } from '@/assets/backend';
 import type { DreamsResponse } from '@/pocketbase-types';
-const userId = pb.authStore.model?.id;
-const reve = await pb.collection('users').getOne('pb.authStore.model?.id')
-console.log(reve)
+const dreams = ref([]);
+const isLoading = ref(true);
+const errorMessage = ref('');
+const fetchDreams = async () => {
+  try {
+    const userId = pb.authStore.model?.id;
+    if (!userId) {
+      throw new Error('Utilisateur non authentifié');
+    }
 
-const dreams = await pb.collection('dreams').getFullList({
-  sorted: {
-    filter: `userId="${userId}"`,
+    const response = await pb.collection('dreams').getFullList({
+      filter: `userId="${userId}"`,
+    });
+
+    dreams.value = response;
+  } catch (error) {
+    console.error('Failed to fetch dreams:', error);
+    errorMessage.value = 'Erreur lors de la récupération des rêves.';
+  } finally {
+    isLoading.value = false;
   }
-})
+};
 
+// Appeler fetchDreams au montage du composant
+onMounted(fetchDreams);
 </script>
 <template>
     <div >
