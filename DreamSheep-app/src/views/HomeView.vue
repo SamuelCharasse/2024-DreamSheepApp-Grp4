4c316255241bca2fbe893ec3222999fb8f53fd7b
Lola Roth <lola.roth@edu.univ-fcomte.fr>
2024-06-10 14:16:22 +0200

    modification de rêve complete

--- DreamSheep-app/src/views/HomeView.vue
+++ DreamSheep-app/src/views/HomeView.vue
@@ -3,8 +3,10 @@ import CardHome from '@/components/CardHome.vue';
 import CardDreamFav from '@/components/CardDreamFav.vue';
 </script>
 <template>
+  <div class="pb-32">
     <div class="login">
       <CardDreamFav/>
       <CardHome />
     </div>
+  </div>
 </template>
