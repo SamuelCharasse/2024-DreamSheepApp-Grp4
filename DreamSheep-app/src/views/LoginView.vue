3541df9703922840c2ad328131d76bbf87fdbc0c
Lola Roth <lola.roth@edu.univ-fcomte.fr>
2024-06-13 13:34:51 +0200

    maj d'un bug trop chelou

--- DreamSheep-app/src/views/LoginView.vue
+++ DreamSheep-app/src/views/LoginView.vue
@@ -1,17 +1,18 @@
 <script setup lang="ts">
 import Pocketbase from "pocketbase";
-const pb = new Pocketbase("http://127.0.0.1:8090");
+const pb = new Pocketbase("http://45.155.169.177:3000");
 const users = {
   username: "string",
   email: "string ",
+  name: "string",
 };
 import { ref } from "vue";
-import { RouterLink, useRouter } from "vue-router";
+import { useRouter } from "vue-router";
 import { logIn } from "@/assets/backend";
 import Button from "@/components/Button.vue";
 import CloudIcon from "@/components/icons/CloudIcon.vue";
 import MoonIcon from "@/components/icons/MoonIcon.vue";
-
+import DreamSheep_Logo_vector from "@/components/icons/DreamSheep_Logo_vector.vue";
 const router = useRouter();
 const email = ref("");
 const password = ref("");
@@ -25,9 +26,9 @@ const handleSignIn = async () => {
     const authData = await logIn(email.value, password.value);
     console.log("authData:", authData); // Ajout de log pour diagnostiquer le problème
     if (authData && authData.token) {
-      localStorage.setItem("authToken", authData.token);
+      localStorage.setItem("authToken", authData.token); // permet de restez en ligne après
       localStorage.setItem("userId", authData.record.id); // Utilisez authData.record.id
-      router.push("/diary");
+      router.push("/home"); //envoie l'utiliseateur à la page d'accueil  si c'est good
     } else {
       throw new Error("Les données de session sont manquantes.");
     }
@@ -41,66 +42,67 @@ const handleSignIn = async () => {
 </script>
 
 <template>
-  <section>
-    <div class="text-white text-center py-8">
-      <h2>Bienvenue sur</h2>
-      <h1>DreamSheep</h1>
-    </div>
-  </section>
-  <div class="m-5">
+  <div class="mx-5 mt-10">
     <form @submit.prevent="handleSignIn">
-      <div class="pb-7 flex flex-col rounded-lg">
-        <label class="text-white text-base font-medium pb-1" for="email"
-          >Email:</label
-        >
-        <input
-          class="text-black rounded-md p-2"
-          type="email"
-          id="email"
-          v-model="email"
-          required
-        />
+      <div class=" text-white">
+        <p class="pb-4 text-center text-xl">Bienvenue sur</p>
+        <div class="flex justify-center items-center gap-6 pb-12">
+          <DreamSheep_Logo_vector />
+          <h2 class="text-lg">DreamSheep</h2>
+        </div>
+        <div class="pb-7 flex flex-col rounded-lg">
+          <label class="text-white text-base font-medium pb-1" for="email"
+            >Email:</label
+          >
+          <input
+            class="text-black rounded-md p-2"
+            type="email"
+            id="email"
+            v-model="email"
+            required
+          />
+        </div>
+        <div>
+          <div class="pb-4 flex flex-col rounded-lg">
+            <label class="text-white text-base font-medium pb-1" for="password"
+              >Mot de passe:</label
+            >
+            <input
+              class="text-black rounded-md p-2"
+              type="password"
+              id="password"
+              v-model="password"
+              required
+            />
+          </div>
+          <div v-if="errorMessage" class="error-message">
+            {{ errorMessage }}
+          </div>
+
+          <div class="flex justify-center pt-8 pb-5">
+            <button
+              type="submit"
+              :disabled="isLoading"
+              class="bg-yellow-200 text-black px-2 py-1 rounded-md"
+            >
+              Se connecter
+            </button>
+          </div>
+        </div>
       </div>
-      <div class="pb-7 flex flex-col rounded-lg">
-        <label class="text-white text-base font-medium pb-1" for="password"
-          >Mot de passe:</label
-        >
-        <input
-          class="text-black rounded-md p-2"
-          type="password"
-          id="password"
-          v-model="password"
-          required
-        />
+
+      <div>
+        <p class="text-white text-center">
+          Pas encore de compte ?
+          <router-link to="/signup" class="text-LightPurple font-medium"
+            >Inscrivez-vous !</router-link
+          >
+        </p>
       </div>
-      <div v-if="errorMessage" class="error-message">
-        {{ errorMessage }}
+      <div class="flex justify-center pt-6">
+        <CloudIcon class="m-2" />
+        <MoonIcon class="m-12" />
       </div>
     </form>
-      <div>
-        <RouterLink to="/forgot-password" class="text-white">
-        Mot de passe oublié ?
-      </RouterLink>
-      </div>
-      <div class="flex justify-center pt-8 pb-5">
-        <button
-          class="text-black bg-yellow-200 px-3 py-2 rounded-md"
-          type="submit"
-          :disabled="isLoading"
-        >
-          Se connecter
-        </button>
-      </div>
-      <p class="text-white text-sm text-center">
-        Vous n'avez pas de compte ?
-        <router-link to="/signup" class="text-LightPurple font-bold"
-          >Inscrivez-vous</router-link
-        >
-      </p>
-   
-    <div class="flex justify-center pt-6">
-      <CloudIcon class="m-2" />
-      <MoonIcon class="m-12" />
-    </div>
   </div>
 </template>
