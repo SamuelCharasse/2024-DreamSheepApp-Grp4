ec7410b9eba201a9b111b89f3a1bfda33f686a13
Lola Roth <lola.roth@edu.univ-fcomte.fr>
2024-06-13 08:25:12 +0200

    page reset password

--- DreamSheep-app/src/views/ResetPasswordView.vue
+++ DreamSheep-app/src/views/ResetPasswordView.vue
@@ -1,41 +1,61 @@
 <script setup lang="ts">
-import { ref } from 'vue';
-import { useRoute, useRouter } from 'vue-router';
-import { pb } from '@/assets/backend';
+import { ref } from "vue";
+import { useRoute, useRouter } from "vue-router";
+import { pb } from "@/assets/backend";
+import DreamSheep_Logo_vector from "@/components/icons/DreamSheep_Logo_vector.vue";
 
 const route = useRoute();
 const router = useRouter();
-const password = ref('');
-const passwordConfirm = ref('');
+const password = ref("");
+const passwordConfirm = ref("");
 
 const handleResetPassword = async () => {
   const token = route.params.token; // Assurez-vous que le token est passé en tant que paramètre de route
   if (password.value !== passwordConfirm.value) {
-    alert('Les mots de passe ne correspondent pas.');
+    alert("Les mots de passe ne correspondent pas.");
     return;
   }
 
   try {
-    await pb.collection('users').confirmPasswordReset(token, password.value, passwordConfirm.value);
-    alert('Votre mot de passe a été réinitialisé avec succès.');
-    router.push('/login');
+    await pb
+      .collection("users")
+      .confirmPasswordReset(token, password.value, passwordConfirm.value);
+    alert("Votre mot de passe a été réinitialisé avec succès.");
+    router.push("/login");
   } catch (error) {
-    console.error('Error resetting password:', error);
-    alert('Erreur lors de la réinitialisation du mot de passe.');
+    console.error("Error resetting password:", error);
+    alert("Erreur lors de la réinitialisation du mot de passe.");
   }
 };
 </script>
 
 <template>
-    <div>
-      <h1>Réinitialiser le mot de passe</h1>
-      <form @submit.prevent="handleResetPassword">
-        <input v-model="password" type="password" placeholder="Nouveau mot de passe" required />
-        <input v-model="passwordConfirm" type="password" placeholder="Confirmez le mot de passe" required />
-        <button type="submit">Réinitialiser le mot de passe</button>
-      </form>
+  <div class="text-white mx-3">
+    <div class="flex justify-center gap-8 mt-8 items-center">
+      <DreamSheep_Logo_vector />
+      <p class="text-lg">Dreamsheep</p>
     </div>
-  </template>
-  
- 
-  
+    <h2 class="text-center mt-20">Réinitialiser votre mot de passe</h2>
+    <form @submit.prevent="handleResetPassword">
+      <div class="flex flex-col mt-4">
+        <input
+          v-model="password"
+          type="password"
+          placeholder="Nouveau mot de passe"
+          required
+          class="mt-6 mb-10 rounded-sm px-2 py-3"
+        />
+        <input
+          v-model="passwordConfirm"
+          type="password"
+          placeholder="Confirmez le mot de passe"
+          required
+          class="mb-4 rounded-sm px-2 py-3"
+        />
+      </div>
+      <div class="flex justify-center mt-8">
+        <button type="submit" class="bg-yellow-200 text-black px-2 py-1 rounded-md">Réinitialiser le mot de passe</button>
+      </div>
+    </form>
+  </div>
+</template>
